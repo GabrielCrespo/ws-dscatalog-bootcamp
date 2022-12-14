1ceb354176a1e0e2322d401b57b5ba772196c3db
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-12-14 20:35:01 -0300

    H2 Database

---
+++ backend/src/main/java/com/gabriel/dscatalog/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.dscatalog.repository;
+
+import com.gabriel.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
