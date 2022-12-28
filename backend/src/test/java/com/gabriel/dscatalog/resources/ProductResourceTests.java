7d2a9d88089626419a8b809f9d9382b65701b9bf
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-12-27 22:55:19 -0300

    Service layer integration tests

--- backend/src/test/java/com/gabriel/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/gabriel/dscatalog/resources/ProductResourceTests.java
@@ -14,8 +14,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
